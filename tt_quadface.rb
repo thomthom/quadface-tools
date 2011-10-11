2b7736c886899046a28c2de2952ac55afd78fcd2
Thomas Thomassen <thomas@thomthom.net>
2011-10-11 12:10:09 +0200

    Changed: Plugin info constants.

--- tt_quadface.rb
+++ tt_quadface.rb
@@ -17,12 +17,11 @@ module TT::Plugins::QuadFaceTools
   ### CONSTANTS ### ------------------------------------------------------------
   
   # Plugin information
-  PLUGIN      = TT::Plugins::QuadFaceTools
-  PLUGIN_ID   = 'TT_QuadFaceTools'.freeze
-  PLUGIN_NAME = 'QuadFace Tools'.freeze
-  #PLUGIN_VERSION     = TT::Version.new(0,1,0).freeze # TT::Lib version 2.6
-  PLUGIN_VERSION     = '0.4.1'.freeze
-  PLUGIN_AUTHOR = 'ThomThom'.freeze
+  PLUGIN          = TT::Plugins::QuadFaceTools
+  PLUGIN_ID       = 'TT_QuadFaceTools'.freeze
+  PLUGIN_NAME     = 'QuadFace Tools'.freeze
+  PLUGIN_VERSION  = '0.4.1'.freeze
+  PLUGIN_AUTHOR   = 'ThomThom'.freeze
   
   # Resource paths
   PATH_ROOT   = File.dirname( __FILE__ ).freeze
