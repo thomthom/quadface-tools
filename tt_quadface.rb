2d1a4f0cc5680da76406a51d7bce4d1f75c12503
Thomas Thomassen <thomas@thomthom.net>
2011-11-13 15:49:42 +0100

    Changed: Updated version and release info

--- tt_quadface.rb
+++ tt_quadface.rb
@@ -20,7 +20,7 @@ module TT::Plugins::QuadFaceTools
   PLUGIN          = TT::Plugins::QuadFaceTools
   PLUGIN_ID       = 'TT_QuadFaceTools'.freeze
   PLUGIN_NAME     = 'QuadFace Tools'.freeze
-  PLUGIN_VERSION  = '0.5.0'.freeze
+  PLUGIN_VERSION  = '0.6.0'.freeze
   PLUGIN_AUTHOR   = 'ThomThom'.freeze
   
   # Resource paths
@@ -52,7 +52,7 @@ module TT::Plugins::QuadFaceTools
       :name => PLUGIN_NAME,
       :author => PLUGIN_AUTHOR,
       :version => PLUGIN_VERSION.to_s,
-      :date => '13 Oct 11',   
+      :date => '13 Nov 11',   
       :description => @ex.description,
       :link_info => 'https://bitbucket.org/thomthom/quadface-tools/'
     }
