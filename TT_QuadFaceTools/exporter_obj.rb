a4d92d8ab428891b4a8e5c3b517712277c76c5b9
Thomas Thomassen <thomas@thomthom.net>
2013-02-27 21:05:46 +0100

    Added: OBJ texture support.

--- TT_QuadFaceTools/exporter_obj.rb
+++ TT_QuadFaceTools/exporter_obj.rb
@@ -7,6 +7,11 @@
 
 module TT::Plugins::QuadFaceTools
 
+  # @see http://en.wikipedia.org/wiki/Wavefront_.obj_file
+  # @see http://www.fileformat.info/format/wavefrontobj/egff.htm
+  # @see http://www.martinreddy.net/gfx/3d/OBJ.spec
+  #
+  # @since 0.8.0
   class ExporterOBJ
 
     EXPORTER_VERSION = '0.1.0'.freeze
@@ -24,10 +29,7 @@ module TT::Plugins::QuadFaceTools
 
     # @since 0.8.0
     def initialize
-      @vertex_index = 1
-      @smoothing_index = 1
-      @materials = {}
-      @last_material = NO_MATERIAL
+      reset()
     end
 
     # @since 0.8.0
@@ -45,9 +47,11 @@ module TT::Plugins::QuadFaceTools
 
     # @since 0.8.0
     def export( filename, group_type = GROUP_BY_OBJECTS )
+      reset()
 
       model = Sketchup.active_model
       name = model_name( model )
+      filename = File.expand_path( filename )
 
       Sketchup.status_text = 'Exporting OBJ file...'
       material_library = material_library_filename( filename )
@@ -70,10 +74,22 @@ module TT::Plugins::QuadFaceTools
       write_material_library( material_library, model, name )
       Sketchup.status_text = 'Done!'
 
+      reset() # Clean up references for GC.
       true
     end
 
     private
+
+    # @since 0.8.0
+    def reset
+      @vertex_index = 1
+      @uvs = TT::JSON.new # UV Index: Key = UV, Value = UV
+
+      @smoothing_index = 1
+
+      @materials = {} # Key = Material, Value = OBJ_Name
+      @last_material = NO_MATERIAL
+    end
 
     # @since 0.8.0
     def write_entities( file, name, native_entities, transformation, group_type )
@@ -150,23 +166,71 @@ module TT::Plugins::QuadFaceTools
     end
 
     # @since 0.8.0
+    def get_uvs( face, outer_loop )
+      if face.is_a?( QuadFace )
+        mapping = face.uv_get( true )
+      else
+        mapping = {}
+        uvh = face.get_UVHelper
+        for vertex in outer_loop
+          next if mapping[ vertex ]
+          uvq = uvh.get_front_UVQ( vertex.position )
+          mapping[ vertex ] = TT::UVQ.normalize( uvq )
+        end
+      end
+      # Convert to arrays with only X and Y values. This avoids the issue of
+      # hashes threating Point3d objects as all unique.
+      uvs = {}
+      for vertex, uv in mapping
+        uvs[ vertex ] = uv.to_a[0..1]
+      end
+      uvs
+    end
+
+    # @since 0.8.0
+    def textured?( face )
+      face.material && face.material.texture ? true : false
+    end
+
+    # @since 0.8.0
     def write_surface( file, surface, transformation, vertices )
       # Collect vertices and faces.
       material_groups = {}
       new_vertices = []
+      new_uvs = []
       for face in surface
         # Build vertex index.
         outer_loop = get_face_loop( face )
         for vertex in outer_loop
-          next if vertices.key?( vertex )
-          new_vertices << vertex
-          vertices[ vertex ] = @vertex_index
-          @vertex_index += 1
+          # Index vertex.
+          unless vertices.key?( vertex )
+            new_vertices << vertex
+            vertices[ vertex ] = @vertex_index
+            @vertex_index += 1
+          end
         end
-        # Build face definition.
-        # (!) Sort by material.
-        #faces << outer_loop.map { |vertex| vertices[ vertex ] }.join(' ')
-        polygon = outer_loop.map { |vertex| vertices[ vertex ] }.join(' ')
+        if textured?( face )
+          # Build UV index
+          face_uv_indexes = {}
+          uvs = get_uvs( face, outer_loop )
+          for vertex, uv in uvs
+            unless index = @uvs[ uv ]
+              new_uvs << uv
+              index = @uvs.size + 1
+              @uvs[ uv ] = index
+            end
+            face_uv_indexes[ vertex ] = index
+          end
+          # Build face definition.
+          polygon = outer_loop.map { |vertex|
+            vp = vertices[ vertex ]
+            vt = face_uv_indexes[ vertex ]
+            "#{vp}/#{vt}"
+          }.join(' ')
+        else
+          # Build face definition.
+          polygon = outer_loop.map { |vertex| vertices[ vertex ] }.join(' ')
+        end
         material_groups[ face.material ] ||= []
         material_groups[ face.material ] << polygon
       end
@@ -185,6 +249,13 @@ module TT::Plugins::QuadFaceTools
         global_position = vertex.position.transform( transformation )
         point = global_position.to_a.join(' ')
         file.puts "v #{point}"
+      end
+
+      # Write UV index.
+      file.puts '' if !new_uvs.empty?
+      for uvs in new_uvs
+        coordinate = uvs.join(' ')
+        file.puts "vt #{coordinate}"
       end
 
       # Write face definitions.
@@ -208,6 +279,11 @@ module TT::Plugins::QuadFaceTools
     def write_material_library( filename, model, modelname )
       return false if @materials.empty?
 
+      # Everything is wrapped in an operation which is aborted because temporary
+      # groups has to be created in order to extract the images.
+      model.start_operation( 'Extract OBJ Textures' )
+      tw = Sketchup.create_texture_writer
+
       File.open( filename, 'wb+' ) { |file|
         sketchup_name = ( Sketchup.is_pro? ) ? 'SketchUp Pro' : 'SketchUp'
         file.puts "# Exported with #{PLUGIN_NAME} (#{PLUGIN_VERSION})"
@@ -220,18 +296,51 @@ module TT::Plugins::QuadFaceTools
           diffuse_color  = format_material_color( color )
           specular_color = sprintf( '%.6f %.6f %.6f', 0.33, 0.33, 0.33 ) # SU values
           opacity        = format_material_opacity( material )
+          texture        = extract_texture( model, tw, material, filename )
           file.puts ''
           file.puts "newmtl #{name}"
           file.puts "Ka #{ambient_color}"
           file.puts "Kd #{diffuse_color}"
           file.puts "Ks #{specular_color}"
           file.puts "d #{opacity}" if opacity
+          file.puts "map_Kd #{texture}" if texture
         end
       }
+      model.abort_operation
 
-      @materials.clear
-      @last_material = NO_MATERIAL
       true
+    end
+
+    # @since 0.8.0
+    def extract_texture( model, tw, material, mtl_filename )
+      return nil if material.nil?
+      return nil if material.texture.nil?
+
+      mtl_path = File.dirname( mtl_filename )
+      texture_path_name = File.basename( mtl_filename, '.mtl' )
+      texture_path = File.join( mtl_path, texture_path_name )
+
+      basename = File.basename( material.texture.filename )
+      texture_filetype = basename.split('.').last
+      texture_basename = File.basename( basename, ".#{texture_filetype}" )
+      texture_OBJ_name = obj_compatible_name( material.name )
+      texture_filename = "#{texture_OBJ_name}.#{texture_filetype}"
+
+      filename = File.join( texture_path, texture_filename )
+      relative_filename = "#{texture_path_name}/#{texture_filename}"
+
+      unless File.exist?( texture_path )
+        Dir.mkdir( texture_path )
+      end
+
+      temp_group = model.entities.add_group
+      temp_group.material = material
+      tw.load( temp_group )
+      unless tw.write( temp_group, filename ) == FILE_WRITE_OK
+        puts "Failed to write #{material.display_name} to #{filename}"
+      end
+
+      relative_filename
     end
 
     # @since 0.8.0
