f33a2a36f6d4c6a5133f4f2fc47ccb98b1408e41
Thomas Thomassen <thomas@thomthom.net>
2015-06-18 21:06:05 +0200

    Adding support for OBJ smoothing groups.

--- src/TT_QuadFaceTools/importers/obj.rb
+++ src/TT_QuadFaceTools/importers/obj.rb
@@ -77,15 +77,26 @@ class ObjImporter < Sketchup::Importer
     base_path = File.dirname(filename)
     model = Sketchup.active_model
     model.start_operation('Import OBJ', true)
+    # The base group containing all the imported entities.
     group = model.active_entities.add_group
     group.name = File.basename(filename)
     root_entities = group.entities
+    # The current Sketchup::Entities collection new entities should be added to.
     entities = root_entities
-    # OBJ files uses 1-based indicies.
+    # Material manager for the OBJ file being parsed.
     materials = MtlParser.new(model, base_path)
+    # The current material which should be applied to geometry.
+    material = nil
+    # List of vertices defined for the OBJ file.
+    # OBJ files uses 1-based indicies.
     vertex_cache = VertexCache.new
     vertex_cache.index_base = 1
-    material = nil
+    # A hash with smoothing group numbers mapping to the faces within the same
+    # smoothing group.
+    smoothing_groups = {}
+    # The current smoothing group new faces should be added to unless its Nil.
+    smoothing_group = nil
+    # Statistics over the imported OBJ data.
     stats = Statistics.new
     Sketchup.status_text = 'Importing OBJ file...'
     # @see http://paulbourke.net/dataformats/obj/
@@ -142,23 +153,26 @@ class ObjImporter < Sketchup::Importer
               mapping << TT::UVQ.normalize(uvw)
             end
           }
-          create_face(entities, points, material, mapping)
+          face = create_face(entities, points, material, mapping)
+          if smoothing_group
+            smoothing_groups[smoothing_group] ||= []
+            smoothing_groups[smoothing_group] << face
+          end
           stats.faces += 1
         when 'o'
           group = root_entities.add_group
           group.name = data[0] unless data[0].empty?
           entities = group.entities
-          next
+          stats.objects += 1
         when 's'
-          # TODO: Smoothing groups.
-          puts line
-          next
+          group_number = data[0] == 'off' ? nil : data[0].to_i
+          group_number = nil if group_number == 0
+          smoothing_group = group_number
         when 'mtllib'
           data.each { |library|
             library_file = find_file(library, filename)
             materials.read(library_file)
           }
-          next
         when 'usemtl'
           material = materials.get(data[0])
         else
@@ -168,6 +182,7 @@ class ObjImporter < Sketchup::Importer
         end
       }
     }
+    apply_smoothing_groups(smoothing_groups)
     model.commit_operation
     Sketchup.status_text = ''
     # Display summary back to the user
@@ -179,6 +194,7 @@ class ObjImporter < Sketchup::Importer
       message << "Lines: #{stats.lines}\n"
       message << "Faces: #{stats.faces}\n"
       message << "Materials: #{materials.used_materials}\n"
+      message << "Smoothing Groups: #{smoothing_groups.size}\n"
       UI.messagebox(message, MB_MULTILINE)
     end
     ImportResult::SUCCESS
@@ -191,13 +207,39 @@ class ObjImporter < Sketchup::Importer
 
   private
 
-  Statistics = Struct.new(:points, :lines, :faces) do
+  Statistics = Struct.new(:points, :lines, :faces, :objects) do
     def initialize(*args)
       super(*args)
-      self.points ||= 0
-      self.lines  ||= 0
-      self.faces  ||= 0
+      self.points  ||= 0
+      self.lines   ||= 0
+      self.faces   ||= 0
+      self.objects ||= 0
     end
+  end
+
+  # @param [Hash{Integer => Array<Sketchup::Face, QuadFace>}]
+  #
+  # @return [Nil]
+  def apply_smoothing_groups(smoothing_groups)
+    smoothing_groups.values.each { |faces|
+      edge_refs = {}
+      # Count how many times each edge is used by the faces in the smoothing
+      # group.
+      faces.each { |face|
+        face.edges.each { |edge|
+          edge_refs[edge] ||= 0
+          edge_refs[edge] += 1
+        }
+      }
+      # Any edge referencing two faces should be smooth. Less and it's at a
+      # border, more and it's part of a fork.
+      edge_refs.each { |edge, refs|
+        next unless refs == 2
+        edge.soft = true
+        edge.smooth = true
+      }
+    }
+    nil
   end
 
   # @param [Sketchup::Entities] entities
