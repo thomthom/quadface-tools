8a8a93b639dda6d0d5fb88ee5986e802281b172c
Thomas Thomassen <thomas@thomthom.net>
2018-10-13 17:48:21 +0200

    Remove unused code.

--- src/TT_QuadFaceTools/tools/slope.rb
+++ src/TT_QuadFaceTools/tools/slope.rb
@@ -5,71 +5,11 @@
 #
 #-------------------------------------------------------------------------------
 
-require 'matrix'
 require 'set'
 
 module TT::Plugins::QuadFaceTools
 
   class SlopeInspectTool
-
-    class Normal
-
-      attr_reader :centroid, :vector, :points
-
-      def self.from_face(face)
-        positions = face.vertices.map(&:position)
-        centroid = TT::Geom3d.average_point(positions)
-        vector = face.normal
-        self.new(centroid, vector)
-      end
-
-      def self.from_quad(quad)
-        self.new(quad.centroid, quad.plane_normal2)
-      end
-
-      def initialize(centroid, vector)
-        @centroid = Geom::Point3d.new(centroid)
-        @vector = Geom::Vector3d.new(vector).normalize
-        @points = normal_points(centroid, vector)
-      end
-
-      SLOPE_SAME = 0
-      SLOPE_TOWARDS = -1
-      SLOPE_AWAY = 1
-
-      def slope_compare(normal)
-        n1, n2 = normal.points
-
-        distance1 = points.first.distance(n1)
-        distance2 = points.last.distance(n2)
-
-        if distance1 == distance2
-          SLOPE_SAME
-        else
-          distance1 > distance2 ? SLOPE_TOWARDS : SLOPE_AWAY
-        end
-      end
-
-      def slope_towards?(normal)
-        slope_compare(normal) == SLOPE_TOWARDS
-      end
-
-      def slope_away?(normal)
-        slope_compare(normal) == SLOPE_AWAY
-      end
-
-      def slope_same?(normal)
-        slope_compare(normal) == SLOPE_SAME
-      end
-
-      private
-
-      def normal_points(centroid, vector)
-        # [centroid, centroid.offset(vector, 100)]
-        [centroid, centroid.offset(vector)]
-      end
-
-    end
 
     class QuadSlope
 
@@ -108,7 +48,6 @@ module TT::Plugins::QuadFaceTools
       end
 
       def vertices
-        # @faces.map(&:vertices).flatten.uniq
         f1, f2 = @faces
         f1.outer_loop.vertices << (f2.vertices - f1.vertices).first
       end
@@ -122,15 +61,8 @@ module TT::Plugins::QuadFaceTools
       end
 
       def edges
-        e1 = sort_edges(@faces[0].outer_loop.edges - [@edge], @faces[0].outer_loop)
-        e2 = sort_edges(@faces[1].outer_loop.edges - [@edge], @faces[1].outer_loop)
-        # e1 + e2
-        # e1 + e2.reverse
-        TT::Edges.sort(e1 + e2)
-
-        # x = @faces.map { |f| f.outer_loop.edges }.flatten.uniq
-        # x.delete(@edge)
-        # x
+        unsorted_edges = @faces.map(&:edges).flatten.uniq - [@edge]
+        TT::Edges.sort(unsorted_edges)
       end
 
       def positions
@@ -157,6 +89,7 @@ module TT::Plugins::QuadFaceTools
 
       def plane_normal
         a, b, c, d = plane
+        # NOTE: Might return a vector in the wrong direction.
         Geom::Vector3d.new(a, b, c).normalize
       end
 
@@ -168,7 +101,6 @@ module TT::Plugins::QuadFaceTools
         pt1, pt2, pt3 = points
         vx = pt1.vector_to(pt2)
         vy = pt1.vector_to(pt3)
-        # vx * vy
         (vx * vy).normalize
       end
 
@@ -189,36 +121,8 @@ module TT::Plugins::QuadFaceTools
         QuadSlope.find(other_face) #| other_face
       end
 
-      def towards?(face1, face2)
-        face1_normal = face1.is_a?(QuadSlope) ? face1.plane_normal2 : face1.normal
-        face2.vertices.map(&:position).any? { |point|
-          plane_point = point.project_to_plane(face1.plane)
-          vector = plane_point.vector_to(point)
-          vector.valid? && vector.samedirection?(face1_normal)
-        }
-      end
-
-      # https://stackoverflow.com/a/15691064/486990
-      def towards_pts?(pts1, pts2)
-        a, b, c, d = Geom.fit_plane_to_points(pts1)
-        pts2.any? { |pt|
-          x, y, z = *pt.to_a
-          dot( [a,b,c,d], [x,y,z,1] ) > 0
-        }
-        # dot( (a,b,c,d), (x,y,z,1) ) > 0
-      end
-
-      def dot(n1, n2)
-        Vector[*n1].inner_product Vector[*n2]
-      end
-
       def average_vector(vectors)
-        average = vectors.inject(Geom::Vector3d.new) { |s, v| s + v }
-        # average.x /= vectors.size
-        # average.y /= vectors.size
-        # average.z /= vectors.size
-        # average
-        average.normalize
+        vectors.inject(Geom::Vector3d.new) { |s, v| s + v }.normalize
       end
 
       def rotate(enumerable, n = 1)
@@ -275,16 +179,6 @@ module TT::Plugins::QuadFaceTools
         raise "Expected 4 edges, got #{edges1.size}" unless edges1.size == 4
         raise "Expected 4 edges, got #{edges2.size}" unless edges2.size == 4
 
-        # p edges1 if debug
-        # p edges2 if debug
-
-        # p edges1.map(&:persistent_id) if debug
-        # p edges2.map(&:persistent_id) if debug
-
-        # puts '> vertices'
-        # p edges1.map(&:vertices).flatten.uniq if debug
-        # p edges2.map(&:vertices).flatten.uniq if debug
-
         average1 = average_deviance(edges1, debug)
         average2 = average_deviance(edges2, debug)
 
@@ -299,41 +193,6 @@ module TT::Plugins::QuadFaceTools
         x_axis = pt1.vector_to(pt2)
         y_axis = pt1.vector_to(pt3)
         (x_axis * y_axis).normalize
-      end
-
-      def highest_vertex
-        # vertices.max { |a, b| a.position.z <=> b.position.z }
-
-        vertices.min { |a, b| a.position.z <=> b.position.z }
-        sorted = vertices.sort { |a, b|
-          a.position.z <=> b.position.z
-        }.last
-
-        # local_vertices.sort { |a, b|
-        #   a.position.z <=> b.position.z
-        # }.last.entity
-      end
-
-      def lowest_vertex
-        vertices.min { |a, b| a.position.z <=> b.position.z }
-        sorted = vertices.sort { |a, b|
-          a.position.z <=> b.position.z
-        }.first
-
-        # local_vertices.sort { |a, b|
-        #   a.position.z <=> b.position.z
-        # }.first.entity
-      end
-
-      def local_vertices
-        origin = diagonal_center
-        z_axis = plane_normal2
-        to_plane = Geom::Transformation.new(origin, z_axis)
-
-        vertices.map { |vertex|
-          pt = vertex.position.transform(to_plane)
-          Vertex.new(vertex, pt)
-        }
       end
 
       def self.find(face1)
@@ -424,10 +283,6 @@ module TT::Plugins::QuadFaceTools
       draw_quads(view)
 
       return unless @quad && @quad.valid?
-
-      # High/Low
-      view.draw_points([@quad.highest_vertex.position], 10, 2, 'red')
-      view.draw_points([@quad.lowest_vertex.position], 10, 2, 'blue')
 
       # Quad
       color = color = @quad.smooth_slope?(true) ? [0, 255, 0, 64] : [255, 0, 0, 64]
